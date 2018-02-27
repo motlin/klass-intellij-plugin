77ca2aa2e7b508406a3459faaa7e8c71d1ebce38
Craig P. Motlin <cmotlin@gmail.com>
2018-02-27 12:47:08 -0500

    Add go-to-class contributor, similar to the go-to-symbol contributor.

--- src/com/klass/intellij/navigation/AbstractKlassChooseByNameContributor.java
+++ src/com/klass/intellij/navigation/AbstractKlassChooseByNameContributor.java
@@ -5,7 +5,6 @@ import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
 import com.klass.intellij.KlassUtil;
 import com.klass.intellij.psi.KlassAssociation;
-import com.klass.intellij.psi.KlassAssociationEnd;
 import com.klass.intellij.psi.KlassClass;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,33 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class KlassChooseByNameContributor implements ChooseByNameContributor
+public abstract class AbstractKlassChooseByNameContributor implements ChooseByNameContributor
 {
-    @NotNull
-    @Override
-    public String[] getNames(Project project, boolean includeNonProjectItems)
+    protected void addAssociationNames(Project project, List<String> names)
     {
-        List<String> names = new ArrayList<>();
-
-        KlassUtil.findClasses(project).stream()
-                .map(KlassClass::getName)
-                .filter(Objects::nonNull)
-                .filter(name -> !name.isEmpty())
-                .forEachOrdered(names::add);
-
         KlassUtil.findAssociations(project).stream()
                 .map(KlassAssociation::getName)
                 .filter(Objects::nonNull)
                 .filter(name -> !name.isEmpty())
                 .forEachOrdered(names::add);
+    }
 
-        KlassUtil.findAssociationEnds(project).stream()
-                .map(KlassAssociationEnd::getName)
+    protected void addClassNames(Project project, List<String> names)
+    {
+        KlassUtil.findClasses(project).stream()
+                .map(KlassClass::getName)
                 .filter(Objects::nonNull)
                 .filter(name -> !name.isEmpty())
                 .forEachOrdered(names::add);
-
-        return names.toArray(new String[names.size()]);
     }
 
     @NotNull
