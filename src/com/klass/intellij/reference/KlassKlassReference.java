0d12ba68d67c208e681565ceb861cebef008c083
Craig P. Motlin <cmotlin@gmail.com>
2018-04-06 09:38:22 -0400

    Fix behavior when there is an ambiguous reference to a class or enumeration.

--- src/com/klass/intellij/reference/KlassKlassReference.java
+++ src/com/klass/intellij/reference/KlassKlassReference.java
@@ -1,5 +1,8 @@
 package com.klass.intellij.reference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.intellij.codeInsight.completion.InsertionContext;
 import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler;
 import com.intellij.codeInsight.lookup.LookupElement;
@@ -7,69 +10,24 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementResolveResult;
+import com.intellij.psi.PsiPolyVariantReference;
+import com.intellij.psi.PsiReferenceBase;
+import com.intellij.psi.ResolveResult;
 import com.klass.intellij.KlassUtil;
-import com.klass.intellij.psi.KlassEnumeration;
 import com.klass.intellij.psi.KlassKlass;
-import com.klass.intellij.psi.KlassNamedElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class KlassKlassReference extends PsiReferenceBase<PsiElement> implements PsiPolyVariantReference
 {
     private final String name;
 
-    private KlassNamedElement namedElement;
-
     public KlassKlassReference(@NotNull PsiElement element, String name)
     {
         super(element, new TextRange(0, name.length()));
         this.name = name;
-    }
-
-    @NotNull
-    @Override
-    public ResolveResult[] multiResolve(boolean incompleteCode)
-    {
-        if (this.namedElement != null)
-        {
-            return new PsiElementResolveResult[]{new PsiElementResolveResult(this.namedElement)};
-        }
-
-        Project project = this.myElement.getProject();
-        List<KlassKlass> klasses = KlassUtil.findClasses(project)
-                .stream()
-                .filter(klass -> klass.getName().equals(this.name))
-                .collect(Collectors.toList());
-        if (klasses.size() == 2)
-        {
-            throw new AssertionError();
-        }
-        if (klasses.size() == 1)
-        {
-            this.namedElement = klasses.get(0);
-            return new PsiElementResolveResult[]{new PsiElementResolveResult(this.namedElement)};
-        }
-
-        List<KlassEnumeration> enumerations1 = KlassUtil.findEnumerations(project);
-        List<KlassEnumeration> enumerations = enumerations1
-                .stream()
-                .filter(enumeration -> enumeration.getName().equals(this.name))
-                .collect(Collectors.toList());
-        if (enumerations.size() == 2)
-        {
-            throw new AssertionError();
-        }
-        if (enumerations.size() == 1)
-        {
-            this.namedElement = enumerations.get(0);
-            return new PsiElementResolveResult[]{new PsiElementResolveResult(this.namedElement)};
-        }
-        return new ResolveResult[]{};
     }
 
     @Nullable
@@ -82,11 +40,39 @@ public class KlassKlassReference extends PsiReferenceBase<PsiElement> implements
 
     @NotNull
     @Override
-    public Object[] getVariants()
+    public ResolveResult[] multiResolve(boolean incompleteCode)
     {
         Project project = this.myElement.getProject();
-        List<KlassKlass> klassKlasses = KlassUtil.findClasses(project);
-        List<LookupElement> variants = new ArrayList<>();
+        ResolveResult[] klassResolveResults = KlassUtil.findClasses(project)
+                .stream()
+                .filter(klass -> klass.getName().equals(this.name))
+                .map(PsiElementResolveResult::new)
+                .toArray(ResolveResult[]::new);
+        if (klassResolveResults.length > 0)
+        {
+            return klassResolveResults;
+        }
+
+        ResolveResult[] enumerationResolveResults = KlassUtil.findEnumerations(project)
+                .stream()
+                .filter(enumeration -> enumeration.getName().equals(this.name))
+                .map(PsiElementResolveResult::new)
+                .toArray(ResolveResult[]::new);
+        if (enumerationResolveResults.length > 0)
+        {
+            return enumerationResolveResults;
+        }
+
+        return new ResolveResult[]{};
+    }
+
+    @NotNull
+    @Override
+    public Object[] getVariants()
+    {
+        Project               project       = this.myElement.getProject();
+        List<KlassKlass>      klassKlasses  = KlassUtil.findClasses(project);
+        List<LookupElement>   variants      = new ArrayList<>();
         BracketsInsertHandler insertHandler = new BracketsInsertHandler();
         for (KlassKlass klassKlass : klassKlasses)
         {
