ae95a254a6c1a7bfa31658443c6234e7186581f3
Craig P. Motlin <cmotlin@gmail.com>
2022-06-02 09:29:09 -0400

    Enhance KlassFoldingBuilder to include all block types, not just class and enumeration.

--- src/com/klass/intellij/folding/KlassFoldingBuilder.java
+++ src/com/klass/intellij/folding/KlassFoldingBuilder.java
@@ -8,17 +8,23 @@ import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.util.Couple;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
-import com.klass.intellij.psi.KlassEnumeration;
-import com.klass.intellij.psi.KlassKlass;
+import com.klass.intellij.psi.KlassAssociationBlock;
+import com.klass.intellij.psi.KlassClassBlock;
+import com.klass.intellij.psi.KlassEnumerationBlock;
+import com.klass.intellij.psi.KlassInterfaceBlock;
 import com.klass.intellij.psi.KlassLBrace;
+import com.klass.intellij.psi.KlassProjectionBlock;
 import com.klass.intellij.psi.KlassRBrace;
+import com.klass.intellij.psi.KlassServiceBlock;
+import com.klass.intellij.psi.KlassServiceGroupBlock;
 import com.klass.intellij.psi.KlassTokenType;
 import com.klass.intellij.psi.KlassTypes;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.tuple.Tuples;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,18 +46,9 @@ public class KlassFoldingBuilder implements FoldingBuilder, DumbAware
     {
         PsiElement element = node.getPsi();
 
-        if (element instanceof KlassKlass && KlassFoldingBuilder.spanMultipleLines(node, document))
+        if (hasBlock(element) && KlassFoldingBuilder.spanMultipleLines(node, document))
         {
-            KlassLBrace lBrace = ((KlassKlass) element).getClassBlock().getLBrace();
-            KlassRBrace rBrace = ((KlassKlass) element).getClassBlock().getRBrace();
-            TextRange textRange = new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
-            descriptors.add(new FoldingDescriptor(node, textRange));
-        }
-        else if (element instanceof KlassEnumeration && KlassFoldingBuilder.spanMultipleLines(node, document))
-        {
-            KlassLBrace lBrace = ((KlassEnumeration) element).getEnumerationBlock().getLBrace();
-            KlassRBrace rBrace = ((KlassEnumeration) element).getEnumerationBlock().getRBrace();
-            TextRange textRange = new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+            TextRange textRange = getTextRange(element);
             descriptors.add(new FoldingDescriptor(node, textRange));
         }
 
@@ -62,9 +59,10 @@ public class KlassFoldingBuilder implements FoldingBuilder, DumbAware
         }
         else if (type == KlassTokenType.END_OF_LINE_COMMENT)
         {
-            Couple<PsiElement> commentRange = KlassFoldingBuilder.expandLineCommentsRange(element);
-            int startOffset = commentRange.getFirst().getTextRange().getStartOffset();
-            int endOffset = commentRange.getSecond().getTextRange().getEndOffset();
+            Pair<PsiElement, PsiElement> commentRange = KlassFoldingBuilder.expandLineCommentsRange(element);
+
+            int startOffset = commentRange.getOne().getTextRange().getStartOffset();
+            int endOffset = commentRange.getTwo().getTextRange().getEndOffset();
             if (document.getLineNumber(startOffset) != document.getLineNumber(endOffset))
             {
                 descriptors.add(new FoldingDescriptor(node, new TextRange(startOffset, endOffset)));
@@ -77,12 +75,84 @@ public class KlassFoldingBuilder implements FoldingBuilder, DumbAware
         }
     }
 
+    private static boolean hasBlock(PsiElement psiElement)
+    {
+        return psiElement instanceof KlassClassBlock
+                || psiElement instanceof KlassInterfaceBlock
+                || psiElement instanceof KlassEnumerationBlock
+                || psiElement instanceof KlassAssociationBlock
+                || psiElement instanceof KlassProjectionBlock
+                || psiElement instanceof KlassServiceGroupBlock
+                || psiElement instanceof KlassServiceBlock;
+    }
+
+    @NotNull
+    private static TextRange getTextRange(@NotNull PsiElement psiElement)
+    {
+        if (psiElement instanceof KlassClassBlock)
+        {
+            KlassLBrace lBrace    = ((KlassClassBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassClassBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassInterfaceBlock)
+        {
+            KlassLBrace lBrace    = ((KlassInterfaceBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassInterfaceBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassEnumerationBlock)
+        {
+            KlassLBrace lBrace    = ((KlassEnumerationBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassEnumerationBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassAssociationBlock)
+        {
+            KlassLBrace lBrace    = ((KlassAssociationBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassAssociationBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassProjectionBlock)
+        {
+            KlassLBrace lBrace    = ((KlassProjectionBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassProjectionBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassServiceGroupBlock)
+        {
+            KlassLBrace lBrace    = ((KlassServiceGroupBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassServiceGroupBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        if (psiElement instanceof KlassServiceBlock)
+        {
+            KlassLBrace lBrace    = ((KlassServiceBlock) psiElement).getLBrace();
+            KlassRBrace rBrace    = ((KlassServiceBlock) psiElement).getRBrace();
+            return new TextRange(lBrace.getTextOffset(), rBrace.getTextOffset() + 1);
+        }
+
+        throw new AssertionError(psiElement);
+    }
+
     @Nullable
     @Override
     public String getPlaceholderText(@NotNull ASTNode node)
     {
         IElementType type = node.getElementType();
-        if (type == KlassTypes.KLASS || type == KlassTypes.ENUMERATION)
+        if (type == KlassTypes.CLASS_BLOCK
+                || type == KlassTypes.INTERFACE_BLOCK
+                || type == KlassTypes.ENUMERATION_BLOCK
+                || type == KlassTypes.ASSOCIATION_BLOCK
+                || type == KlassTypes.PROJECTION_BLOCK
+                || type == KlassTypes.SERVICE_GROUP_BLOCK
+                || type == KlassTypes.SERVICE_BLOCK)
         {
             return "{...}";
         }
@@ -104,9 +174,9 @@ public class KlassFoldingBuilder implements FoldingBuilder, DumbAware
     }
 
     @NotNull
-    public static Couple<PsiElement> expandLineCommentsRange(@NotNull PsiElement anchor)
+    public static Pair<PsiElement, PsiElement> expandLineCommentsRange(@NotNull PsiElement anchor)
     {
-        return Couple.of(KlassFoldingBuilder.findFurthestSiblingOfSameType(anchor, false), KlassFoldingBuilder.findFurthestSiblingOfSameType(anchor, true));
+        return Tuples.pair(KlassFoldingBuilder.findFurthestSiblingOfSameType(anchor, false), KlassFoldingBuilder.findFurthestSiblingOfSameType(anchor, true));
     }
 
     @NotNull
