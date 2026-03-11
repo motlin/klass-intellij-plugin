6477998f8ef3526da2da0717f1ff2350877d1131
Craig P. Motlin <cmotlin@gmail.com>
2026-03-10 21:36:34 -0400

    Fix checkstyle ConstantName violations.

--- src/main/java/com/klass/intellij/highlighter/KlassSyntaxHighlighter.java
+++ src/main/java/com/klass/intellij/highlighter/KlassSyntaxHighlighter.java
@@ -17,65 +17,65 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 public class KlassSyntaxHighlighter extends SyntaxHighlighterBase {
-  private static final Map<IElementType, TextAttributesKey> ourMap1;
-  private static final Map<IElementType, TextAttributesKey> ourMap2;
+  private static final Map<IElementType, TextAttributesKey> OUR_MAP_1;
+  private static final Map<IElementType, TextAttributesKey> OUR_MAP_2;
 
   static {
-    ourMap1 = new HashMap<>();
-    ourMap2 = new HashMap<>();
+    OUR_MAP_1 = new HashMap<>();
+    OUR_MAP_2 = new HashMap<>();
 
     SyntaxHighlighterBase.fillMap(
-        ourMap1, TokenSets.KEYWORD_BIT_SET, KlassHighlightingColors.KEYWORD);
+        OUR_MAP_1, TokenSets.KEYWORD_BIT_SET, KlassHighlightingColors.KEYWORD);
     // TODO: Separate color for primitive types?
     SyntaxHighlighterBase.fillMap(
-        ourMap1, TokenSets.PRIMITIVE_TYPE_BIT_SET, KlassHighlightingColors.KEYWORD);
+        OUR_MAP_1, TokenSets.PRIMITIVE_TYPE_BIT_SET, KlassHighlightingColors.KEYWORD);
     SyntaxHighlighterBase.fillMap(
-        ourMap1, TokenSets.LITERAL_BIT_SET, KlassHighlightingColors.KEYWORD);
+        OUR_MAP_1, TokenSets.LITERAL_BIT_SET, KlassHighlightingColors.KEYWORD);
     SyntaxHighlighterBase.fillMap(
-        ourMap1, TokenSets.KEYWORD_OPERATION_BIT_SET, KlassHighlightingColors.KEYWORD);
+        OUR_MAP_1, TokenSets.KEYWORD_OPERATION_BIT_SET, KlassHighlightingColors.KEYWORD);
     SyntaxHighlighterBase.fillMap(
-        ourMap1, TokenSets.OPERATION_BIT_SET, KlassHighlightingColors.OPERATION_SIGN);
+        OUR_MAP_1, TokenSets.OPERATION_BIT_SET, KlassHighlightingColors.OPERATION_SIGN);
 
-    ourMap1.put(XmlTokenType.XML_DATA_CHARACTERS, KlassHighlightingColors.DOC_COMMENT);
-    ourMap1.put(XmlTokenType.XML_REAL_WHITE_SPACE, KlassHighlightingColors.DOC_COMMENT);
-    ourMap1.put(XmlTokenType.TAG_WHITE_SPACE, KlassHighlightingColors.DOC_COMMENT);
+    OUR_MAP_1.put(XmlTokenType.XML_DATA_CHARACTERS, KlassHighlightingColors.DOC_COMMENT);
+    OUR_MAP_1.put(XmlTokenType.XML_REAL_WHITE_SPACE, KlassHighlightingColors.DOC_COMMENT);
+    OUR_MAP_1.put(XmlTokenType.TAG_WHITE_SPACE, KlassHighlightingColors.DOC_COMMENT);
 
     // TODO: ASTERISK is used for multiplicities, but maybe multiplicities should be different than
     // other numbers
-    ourMap1.put(KlassTypes.ASTERISK, KlassHighlightingColors.NUMBER);
-    ourMap1.put(KlassTypes.INTEGER_LITERAL, KlassHighlightingColors.NUMBER);
-    ourMap1.put(KlassTypes.FLOAT_LITERAL, KlassHighlightingColors.NUMBER);
-    ourMap1.put(KlassTypes.STRING_LITERAL, KlassHighlightingColors.STRING);
-    ourMap1.put(
+    OUR_MAP_1.put(KlassTypes.ASTERISK, KlassHighlightingColors.NUMBER);
+    OUR_MAP_1.put(KlassTypes.INTEGER_LITERAL, KlassHighlightingColors.NUMBER);
+    OUR_MAP_1.put(KlassTypes.FLOAT_LITERAL, KlassHighlightingColors.NUMBER);
+    OUR_MAP_1.put(KlassTypes.STRING_LITERAL, KlassHighlightingColors.STRING);
+    OUR_MAP_1.put(
         StringEscapesTokenTypes.VALID_STRING_ESCAPE_TOKEN,
         KlassHighlightingColors.VALID_STRING_ESCAPE);
-    ourMap1.put(
+    OUR_MAP_1.put(
         StringEscapesTokenTypes.INVALID_CHARACTER_ESCAPE_TOKEN,
         KlassHighlightingColors.INVALID_STRING_ESCAPE);
-    ourMap1.put(
+    OUR_MAP_1.put(
         StringEscapesTokenTypes.INVALID_UNICODE_ESCAPE_TOKEN,
         KlassHighlightingColors.INVALID_STRING_ESCAPE);
-    //        ourMap1.put(KlassTypes.CHARACTER_LITERAL, KlassHighlightingColors.STRING);
+    //        OUR_MAP_1.put(KlassTypes.CHARACTER_LITERAL, KlassHighlightingColors.STRING);
 
-    ourMap1.put(KlassTypes.LPAREN, KlassHighlightingColors.PARENTHESES);
-    ourMap1.put(KlassTypes.RPAREN, KlassHighlightingColors.PARENTHESES);
+    OUR_MAP_1.put(KlassTypes.LPAREN, KlassHighlightingColors.PARENTHESES);
+    OUR_MAP_1.put(KlassTypes.RPAREN, KlassHighlightingColors.PARENTHESES);
 
-    ourMap1.put(KlassTypes.LBRACE, KlassHighlightingColors.BRACES);
-    ourMap1.put(KlassTypes.RBRACE, KlassHighlightingColors.BRACES);
+    OUR_MAP_1.put(KlassTypes.LBRACE, KlassHighlightingColors.BRACES);
+    OUR_MAP_1.put(KlassTypes.RBRACE, KlassHighlightingColors.BRACES);
 
-    ourMap1.put(KlassTypes.LBRACKET, KlassHighlightingColors.BRACKETS);
-    ourMap1.put(KlassTypes.RBRACKET, KlassHighlightingColors.BRACKETS);
+    OUR_MAP_1.put(KlassTypes.LBRACKET, KlassHighlightingColors.BRACKETS);
+    OUR_MAP_1.put(KlassTypes.RBRACKET, KlassHighlightingColors.BRACKETS);
 
-    ourMap1.put(KlassTypes.COMMA, KlassHighlightingColors.COMMA);
-    ourMap1.put(KlassTypes.DOT, KlassHighlightingColors.DOT);
-    ourMap1.put(KlassTypes.DOTDOT, KlassHighlightingColors.DOT);
-    ourMap1.put(KlassTypes.SEMICOLON, KlassHighlightingColors.KLASS_SEMICOLON);
-    ourMap1.put(KlassTypes.SLASH, KlassHighlightingColors.DOT);
-    ourMap1.put(KlassTypes.URL_IDENTIFIER, KlassHighlightingColors.URL_CONSTANT);
+    OUR_MAP_1.put(KlassTypes.COMMA, KlassHighlightingColors.COMMA);
+    OUR_MAP_1.put(KlassTypes.DOT, KlassHighlightingColors.DOT);
+    OUR_MAP_1.put(KlassTypes.DOTDOT, KlassHighlightingColors.DOT);
+    OUR_MAP_1.put(KlassTypes.SEMICOLON, KlassHighlightingColors.KLASS_SEMICOLON);
+    OUR_MAP_1.put(KlassTypes.SLASH, KlassHighlightingColors.DOT);
+    OUR_MAP_1.put(KlassTypes.URL_IDENTIFIER, KlassHighlightingColors.URL_CONSTANT);
 
-    ourMap1.put(KlassTokenType.C_STYLE_COMMENT, KlassHighlightingColors.KLASS_BLOCK_COMMENT);
-    ourMap1.put(KlassTokenType.END_OF_LINE_COMMENT, KlassHighlightingColors.LINE_COMMENT);
-    ourMap1.put(TokenType.BAD_CHARACTER, HighlighterColors.BAD_CHARACTER);
+    OUR_MAP_1.put(KlassTokenType.C_STYLE_COMMENT, KlassHighlightingColors.KLASS_BLOCK_COMMENT);
+    OUR_MAP_1.put(KlassTokenType.END_OF_LINE_COMMENT, KlassHighlightingColors.LINE_COMMENT);
+    OUR_MAP_1.put(TokenType.BAD_CHARACTER, HighlighterColors.BAD_CHARACTER);
 
     IElementType[] klassDocMarkup = {
       XmlTokenType.XML_START_TAG_START,
@@ -93,8 +93,8 @@ public class KlassSyntaxHighlighter extends SyntaxHighlighterBase {
       XmlTokenType.XML_EQ,
     };
     for (IElementType idx : klassDocMarkup) {
-      ourMap1.put(idx, KlassHighlightingColors.DOC_COMMENT);
-      ourMap2.put(idx, KlassHighlightingColors.DOC_COMMENT_MARKUP);
+      OUR_MAP_1.put(idx, KlassHighlightingColors.DOC_COMMENT);
+      OUR_MAP_2.put(idx, KlassHighlightingColors.DOC_COMMENT_MARKUP);
     }
   }
 
@@ -105,6 +105,6 @@ public class KlassSyntaxHighlighter extends SyntaxHighlighterBase {
 
   @NotNull @Override
   public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
-    return SyntaxHighlighterBase.pack(ourMap1.get(tokenType), ourMap2.get(tokenType));
+    return SyntaxHighlighterBase.pack(OUR_MAP_1.get(tokenType), OUR_MAP_2.get(tokenType));
   }
 }
